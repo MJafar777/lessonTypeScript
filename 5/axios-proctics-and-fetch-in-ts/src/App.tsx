e1153195ef6bf8047f065ed620d3ea1d6c4cf1a0
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-05-12 16:45:31 +0500

    add user axios post

--- 5/axios-proctics-and-fetch-in-ts/src/App.tsx
+++ 5/axios-proctics-and-fetch-in-ts/src/App.tsx
@@ -13,6 +13,7 @@ const App = () => {
   const [error, setError] = useState<string>();
   const [loading, setLoading] = useState(false);
 
+  // get API function started
   useEffect(() => {
     setLoading(true);
     axios
@@ -26,6 +27,8 @@ const App = () => {
         setLoading(false);
       });
   }, []);
+
+  // get API function finished
 
   // delete function started
   const deleteUser = (user: User) => {
@@ -41,18 +44,40 @@ const App = () => {
   };
   // delete function finished
 
+  // Add user function started
+  const addUser = () => {
+    const originalUser = users && [...users];
+
+    const newUser = { id: 0, name: "Jafar" };
+
+    setUsers(users && [newUser, ...users]);
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers(users && [savedUser, ...users]))
+      .catch((err) => setError(err.message));
+    setUsers(originalUser);
+  };
+  // Add user function finished
+
   return (
     <div>
       <h1>Users json server</h1>
+
       {error && <p className="text-danger">{error}</p>}
+
       {loading && <div className="spinner-border"></div>}
+
+      <button className="btn-primary btn mb-3" onClick={addUser}>
+        Add
+      </button>
+
       {users?.map((user) => {
         return (
           <div
             className="list-group-item d-flex justify-content-between"
             key={user.id}
           >
-            <h2>{user.name}</h2> <h3>{user.phone}</h3>{" "}
+            <h2>{user.name}</h2>{" "}
             <button
               className="btn btn-outline-danger"
               onClick={() => deleteUser(user)}
