4086f6a2090baa5aaba115a33381ffd5f89bbf89
MJafar777 <mirzaraximovjafar2001@gmail.com>
2023-05-12 10:34:09 +0500

    axios

--- 5/axios-proctics-and-fetch-in-ts/src/App.tsx
+++ 5/axios-proctics-and-fetch-in-ts/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  interface User {
+    id: number;
+    name: string;
+    phone: string;
+    usernames: string;
+  }
+
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setUsers(response.data);
+      });
+    return;
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Users json server</h1>
+      {users?.map((user) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+            key={user.id}
+          >
+            <h2>{user.name}</h2> <h3>{user.phone}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
